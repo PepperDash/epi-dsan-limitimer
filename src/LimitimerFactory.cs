0cc2b5f253e40c1174fd21cb4271cbe13890ac3c
Erik Meyer <emeyer@pepperdash.com>
2025-07-30 17:28:57 -0400

    chore: clean up LimitimerFactory code by removing redundant type names and improving logging structure

--- src/LimitimerFactory.cs
+++ src/LimitimerFactory.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using PepperDash.Core;
 using PepperDash.Essentials.Core;
 using PepperDash.Essentials.Core.Config;
@@ -13,30 +14,37 @@ namespace PepperDash.Essentials.Plugins.Limitimer
         {
 			MinimumEssentialsFrameworkVersion = "2.12.1";
 
-            TypeNames = new List<string>() { "Limitimer", "limitimer" };
+            TypeNames = new List<string>() { "limitimer" };
         }
-        
+
         public override EssentialsDevice BuildDevice(DeviceConfig dc)
         {
-            Debug.LogMessage(Serilog.Events.LogEventLevel.Information, "[{key}] Factory Attempting to create new device from type: {type}", this, dc.Key, dc.Type);
+            try {
+                Debug.LogMessage(Serilog.Events.LogEventLevel.Information, "[{key}] Factory Attempting to create new device from type: {type}", this, dc.Key, dc.Type);
 
-            // attempt build the plugin device comms device
-            var comms = CommFactory.CreateCommForDevice(dc);
-            if (comms == null)
+                // attempt build the plugin device comms device
+                var comms = CommFactory.CreateCommForDevice(dc);
+                if (comms == null)
+                {
+                    Debug.LogMessage(Serilog.Events.LogEventLevel.Information, "[{0}] Factory Notice: No control object present for device {1}", dc.Key, dc.Name);
+                    return null;
+                }
+
+                // get the plugin device properties configuration object & check for null 
+                var config = dc.Properties.ToObject<LimitimerPropertiesConfig>();
+                if (config == null)
+                {
+                    Debug.LogMessage(Serilog.Events.LogEventLevel.Information, "[{0}] Factory: failed to read properties config for {1}", dc.Key, dc.Name);
+                    return null;
+                }
+
+                return new LimitimerDevice(dc.Key, dc.Name, config, comms);
+            } 
+            catch (Exception e)
             {
-                Debug.LogMessage(Serilog.Events.LogEventLevel.Information, "[{0}] Factory Notice: No control object present for device {1}", dc.Key, dc.Name);
+                Debug.LogMessage(Serilog.Events.LogEventLevel.Error, "[{key}] Factory Error: Failed to create Limitimer device {name} of type {type}. Exception Type: {exceptionType}, Message: {message}, StackTrace: {stackTrace}", this, dc.Key, dc.Name, dc.Type, e.GetType().Name, e.Message, e.StackTrace);
                 return null;
             }
-
-            // get the plugin device properties configuration object & check for null 
-            var config = dc.Properties.ToObject<LimitimerPropertiesConfig>();
-            if (config == null)
-            {
-                Debug.LogMessage(Serilog.Events.LogEventLevel.Information, "[{0}] Factory: failed to read properties config for {1}", dc.Key, dc.Name);
-                return null;
-            }
-            
-            return new LimitimerDevice(dc.Key, dc.Name, config, comms);
         }
     }
 }
